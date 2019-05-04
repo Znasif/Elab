6b9490e3f33fe0b3084f56b47810a56a7c1e6566
Rokkhi <rokkhi@Rokkhis-MacBook-Pro.local>
2019-05-04 13:58:26 +0600

    2

--- push.sh
+++ push.sh
@@ -1,3 +1,20 @@
 docker build -t znasif/elab .
 heroku container:push web --app elab-ai
 heroku container:release web --app elab-ai
+
+# conda install -c conda-forge -y \
+#     conda-build \
+#     flask \
+#     flask-cors \
+#     flask-restful \
+#     flask-sqlalchemy \
+#     google-cloud-bigquery \
+#     google-cloud-storage \
+#     passlib \
+#     pandas \
+#     keras \
+#     tensorflow \
+#   && conda clean --yes --tarballs --packages \
+#   && conda build purge-all
+
+# pip install --no-cache-dir firebase-admin
